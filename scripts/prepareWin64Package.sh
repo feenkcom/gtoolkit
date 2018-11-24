cf672f8a24c492988a5e3f86cc73b939d5f25e5d
George Ganea <georgeganea@gmail.com>
2018-11-24 17:53:39 +0200

    moved windows to 64

---
+++ scripts/prepareWin64Package.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+curl https://dl.feenk.com/gt/GToolkit64.zip -o GToolkit64.zip
+unzip -u GToolkit64.zip
+mkdir -p GToolkitWin64
+cp -rv GToolkit-64*/* GToolkitWin64
+
+curl http://files.pharo.org/get-files/70/pharo64-win-latest.zip -o pharo64-win-latest.zip
+unzip pharo64-win-latest.zip
+mv -fv pharo64-win-latest/* GToolkitWin64/
+zip -qyr GToolkitWin64.zip GToolkitWin64
+scp GToolkitWin64.zip ubuntu@ec2-35-157-37-37.eu-central-1.compute.amazonaws.com:/var/www/html/gt/
