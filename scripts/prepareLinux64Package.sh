10d91279fe6e47c0f1371907f22c3ab623de4883
George Ganea <georgeganea@gmail.com>
2018-11-24 12:03:01 +0200

    package for linux in parralel

---
+++ scripts/prepareLinux64Package.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+curl https://dl.feenk.com/gt/GToolkit64.zip -o GToolkit64.zip
+unzip -u GToolkit64.zip
+mkdir -p GToolkit64Linux
+cp -r GToolkit-64*/ GToolkit64Linux
+
+curl https://get.pharo.org/64/vmLatest70 | bash
+mv -fv pharo-ui GToolkit64Linux/
+mv -fv pharo GToolkit64Linux/
+mv -fv pharo-vm GToolkit64Linux/
+zip -qyr GToolkit64Linux.zip GToolkit64Linux
+scp GToolkit64Linux.zip ubuntu@ec2-35-157-37-37.eu-central-1.compute.amazonaws.com:/var/www/html/gt/
