5a42e6b9553a56edf06289180165b76cf40359aa
George Ganea <georgeganea@gmail.com>
2018-11-24 13:37:55 +0200

    remove default image

--- scripts/prepareWin32Package.sh
+++ scripts/prepareWin32Package.sh
@@ -8,5 +8,7 @@ cp -rv GToolkit-32*/* GToolkitWin32
 curl https://files.pharo.org/platform/Pharo6.1-win.zip -o Pharo6.1-win.zip
 unzip Pharo6.1-win.zip
 mv -fv Pharo6.1/* GToolkitWin32/
+rm GToolkitWin32/Pharo6.1.image
+rm GToolkitWin32/Pharo6.1.changes
 zip -qyr GToolkitWin32.zip GToolkitWin32
 scp GToolkitWin32.zip ubuntu@ec2-35-157-37-37.eu-central-1.compute.amazonaws.com:/var/www/html/gt/
