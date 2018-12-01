f4baafc76f022a44ee1df0b332a29b20099ee302
George Ganea <georgeganea@gmail.com>
2018-12-01 21:13:07 +0200

    try with pharo-ui again

--- scripts/build/main.sh
+++ scripts/build/main.sh
@@ -4,4 +4,4 @@ echo $DISPLAY
 export DISPLAY=:99.0
 curl https://get.pharo.org/64/alpha+vmLatest | bash
 ./pharo Pharo.image st --save --quit scripts/build/loadgt.st
-./pharo Pharo.image examples --junit-xml-output Bloc 'Bloc-.*' 'GToolkit-.*'
+./pharo-ui Pharo.image examples --junit-xml-output Bloc 'Bloc-.*' 'GToolkit-.*'
