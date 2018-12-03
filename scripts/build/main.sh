5f4d9de50cf4daf26d23a9ab5afe0a5e95d05d24
George Ganea <georgeganea@gmail.com>
2018-12-03 14:49:26 +0200

    only run gtoolkit examples

--- scripts/build/main.sh
+++ scripts/build/main.sh
@@ -4,5 +4,5 @@ echo $DISPLAY
 export DISPLAY=:99.0
 curl https://get.pharo.org/64/alpha+vm | bash
 ./pharo Pharo.image st --quit scripts/build/loadgt.st
-./pharo-ui Pharo.image examples --junit-xml-output Bloc 'Bloc-.*' 'GToolkit-.*'
+./pharo-ui Pharo.image examples --junit-xml-output 'GToolkit-.*'
 exit 0
