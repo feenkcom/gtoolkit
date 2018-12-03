568cc8a7be4ed5a478699a393f7e12f673cd532c
George Ganea <georgeganea@gmail.com>
2018-12-03 15:24:35 +0200

    prepare gtoolkit64 zip

--- scripts/build/main.sh
+++ scripts/build/main.sh
@@ -4,5 +4,17 @@ echo $DISPLAY
 export DISPLAY=:99.0
 curl https://get.pharo.org/64/alpha+vm | bash
 ./pharo Pharo.image st --quit scripts/build/loadgt.st
+export PROJECT_NAME="GToolkit-${BITS}-$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)"
+# customize the name of the build folder
+export ARTIFACT_DIR="${PROJECT_NAME}"
+mkdir "$ARTIFACT_DIR"
+cp Pharo.image "${ARTIFACT_DIR}/${PROJECT_NAME}64.image"
+cp Pharo.changes "${ARTIFACT_DIR}/${PROJECT_NAME}64.changes"
+cp *.sources "${ARTIFACT_DIR}/"
+cp -Rv gt-extra "${ARTIFACT_DIR}/"
+sh ../installMozz2d.sh
+export build_zip="${ARTIFACT_DIR}.zip"
+zip -qr "$build_zip" "$ARTIFACT_DIR"
+cp "$build_zip" "GToolkit64".zip
 ./pharo-ui Pharo.image examples --junit-xml-output 'GToolkit-.*'
 exit 0
