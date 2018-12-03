fffeaa0edb999883fa465a97da5a28592918cf3b
George Ganea <georgeganea@gmail.com>
2018-12-03 22:21:14 +0200

    load the latest vm

--- scripts/build/load.sh
+++ scripts/build/load.sh
@@ -2,6 +2,6 @@
 set -o xtrace
 echo $DISPLAY
 export DISPLAY=:99.0
-curl https://get.pharo.org/64/alpha+vm | bash
+curl https://get.pharo.org/64/alpha+vmLatest | bash
 ./pharo Pharo.image st --quit scripts/build/loadgt.st
 exit 0
