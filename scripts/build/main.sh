f05fc69af81a073800bf418a55d497ba4ee16daa
George Ganea <georgeganea@gmail.com>
2018-12-03 13:31:15 +0200

    try without the save flag

--- scripts/build/main.sh
+++ scripts/build/main.sh
@@ -3,5 +3,5 @@ set -o xtrace
 echo $DISPLAY
 export DISPLAY=:99.0
 curl https://get.pharo.org/64/alpha+vm | bash
-./pharo Pharo.image st --save --quit scripts/build/loadgt.st
-./pharo-ui Pharo.image examples --junit-xml-output Bloc "Bloc-.*"
+./pharo Pharo.image st --quit scripts/build/loadgt.st
+./pharo-ui Pharo.image examples --junit-xml-output Bloc
