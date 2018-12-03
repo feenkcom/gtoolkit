6e4fd6584cfb006bb005ba6e3326aa4ef6cd3c37
George Ganea <georgeganea@gmail.com>
2018-12-03 20:54:26 +0200

    fixed osx archive

--- scripts/build/prepareOSXPackage.sh
+++ scripts/build/prepareOSXPackage.sh
@@ -3,7 +3,7 @@ set -e
 #curl https://dl.feenk.com/gt/GToolkit64.zip -o GToolkit64.zip
 #unzip -u GToolkit64.zip
 mkdir -p GToolkitOSX64
-cp -r GToolkit-64*/ GToolkitOSX64
+cp -r GToolkit-64*/* GToolkitOSX64
 
 curl https://files.pharo.org/get-files/70/pharo64-mac-latest.zip -o pharo64-mac-latest.zip
 unzip pharo64-mac-latest.zip -d pharo64-mac-latest
