60a69515d2202b066e17e7679add03b69ea4847f
George Ganea <georgeganea@gmail.com>
2019-02-05 00:11:40 +0200

    create links with a .zip ending

--- scripts/build/update-latest-links.sh
+++ scripts/build/update-latest-links.sh
@@ -1,9 +1,9 @@
 set -o xtrace
 set -e
 export GTfolder=/var/www/html/gt
-ln -fvs `ls -rt $GTfolder/GToolkitWin64*.zip | tail -n 1` $GTfolder/GToolkitWin64-release
-ln -fvs `ls -rt $GTfolder/GToolkitOSX64*.zip | tail -n 1` $GTfolder/GToolkitOSX64-release
-ln -fvs `ls -rt $GTfolder/GToolkitLinux64*.zip | tail -n 1` $GTfolder/GToolkitLinux64-release
-ln -fvs `ls -rt $GTfolder/GToolkit-64-*.zip | tail -n 1` $GTfolder/GToolkit-latest-build
-ln -fvs `ls -rt $GTfolder/GToolkit-64-v*.zip | tail -n 1` $GTfolder/GToolkit-latest-tag
+ln -fvs `ls -rt $GTfolder/GToolkitWin64*.zip | tail -n 1` $GTfolder/GToolkitWin64-release.zip
+ln -fvs `ls -rt $GTfolder/GToolkitOSX64*.zip | tail -n 1` $GTfolder/GToolkitOSX64-release.zip
+ln -fvs `ls -rt $GTfolder/GToolkitLinux64*.zip | tail -n 1` $GTfolder/GToolkitLinux64-release.zip
+ln -fvs `ls -rt $GTfolder/GToolkit-64-*.zip | tail -n 1` $GTfolder/GToolkit-latest-build.zip
+ln -fvs `ls -rt $GTfolder/GToolkit-64-v*.zip | tail -n 1` $GTfolder/GToolkit-latest-tag.zip
 set +e
