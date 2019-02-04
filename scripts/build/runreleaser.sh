e0acfae211800a1da696b77d9ca23d427422d8c7
George Ganea <georgeganea@gmail.com>
2019-02-04 14:13:32 +0200

    run releaser with command line version as ui version waits for author info

--- scripts/build/runreleaser.sh
+++ scripts/build/runreleaser.sh
@@ -3,5 +3,5 @@ set -o xtrace
 echo $DISPLAY
 export DISPLAY=:99.0
 ./pharo Pharo.image st --quit scripts/build/icebergconfig.st  2>&1
-./pharo-ui Pharo.image releasegtoolkit ${FORCED_TAG_NAME}
+./pharo Pharo.image releasegtoolkit ${FORCED_TAG_NAME}
 exit 0
