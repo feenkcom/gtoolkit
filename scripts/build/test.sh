1df7e3a51eeb269e20fa4667683f03f529a8980d
Andrei Chiș <chisvasileandrei@gmail.com>
2019-04-30 20:51:12 +0300

    Run GtGtoolkitArchitecturalReport on the CI (closes feenkcom/gtoolkit#286)

--- scripts/build/test.sh
+++ scripts/build/test.sh
@@ -32,5 +32,6 @@ set +e
 git config --global user.name "Jenkins"
 git config --global user.email "jenkins@feenk.com"
 ./pharo Pharo.image examples --junit-xml-output 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Starta-.*' 2>&1
+./pharo Pharo.image gtexportreport --report=GtGtoolkitArchitecturalReport
 
 exit 0
