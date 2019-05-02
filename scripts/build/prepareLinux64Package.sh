ddfb62f38274dae75d7edcfc587374f5c9701869
George Ganea <georgeganea@gmail.com>
2019-05-02 12:48:58 +0300

    added libglutin to built image

--- scripts/build/prepareLinux64Package.sh
+++ scripts/build/prepareLinux64Package.sh
@@ -8,6 +8,10 @@ rm -rf $GTFolder/pharo-local
 curl https://files.pharo.org/get-files/70/pharo64-linux-stable.zip -o pharo64-linux-stable.zip
 unzip pharo64-linux-stable.zip -d pharo64-linux-stable
 mv -fv pharo64-linux-stable/* $GTFolder/
+
+curl https://dl.feenk.com/Glutin/linux/development/x86_64/libGlutin.so -o libGlutin.so
+mv libGlutin.so $GTFolder
+
 zip -qyr $GTFolder.zip $GTFolder
 rm -rf $GTFolder
 set +e
--- scripts/build/prepareWin64Package.sh
+++ scripts/build/prepareWin64Package.sh
@@ -8,6 +8,10 @@ rm -rf $GTFolder/pharo-local
 curl http://files.pharo.org/get-files/70/pharo64-win-stable.zip -o pharo64-win-stable.zip
 unzip pharo64-win-stable.zip -d pharo64-win-stable
 mv -fv pharo64-win-stable/* $GTFolder/
+
+curl https://dl.feenk.com/Glutin/windows/development/x86_64/libGlutin.dll -o libGlutin.dll
+mv libGlutin.dll $GTFolder
+
 zip -qyr $GTFolder.zip $GTFolder
 rm -rf $GTFolder
 set +e
