4453cb203fd635ed7a11ffac3450c1d8e88d5563
George Ganea <georgeganea@gmail.com>
2019-05-11 16:32:48 +0300

    #322 added ms redistributable libs to win package

--- scripts/build/prepareWin64Package.sh
+++ scripts/build/prepareWin64Package.sh
@@ -10,7 +10,11 @@ unzip pharo64-win-stable.zip -d pharo64-win-stable
 mv -fv pharo64-win-stable/* $GTFolder/
 
 curl https://dl.feenk.com/Glutin/windows/development/x86_64/libGlutin.dll -o libGlutin.dll
+curl https://dl.feenk.com/Glutin/windows/development/x86_64/msvcp140.dll -o msvcp140.dll
+curl https://dl.feenk.com/Glutin/windows/development/x86_64/vcruntime140.dll -o vcruntime140.dll
 mv libGlutin.dll $GTFolder
+mv msvcp140.dll $GTFolder
+mv vcruntime140.dll $GTFolder
 
 zip -qyr $GTFolder.zip $GTFolder
 rm -rf $GTFolder
