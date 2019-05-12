1625e651aeafa578ae50491e040e4e3ef459ccaa
George Ganea <georgeganea@gmail.com>
2019-05-12 10:28:59 +0300

    fixed url to win redistributables

--- scripts/build/prepareWin64Package.sh
+++ scripts/build/prepareWin64Package.sh
@@ -10,8 +10,8 @@ unzip pharo64-win-stable.zip -d pharo64-win-stable
 mv -fv pharo64-win-stable/* $GTFolder/
 
 curl https://dl.feenk.com/Glutin/windows/development/x86_64/libGlutin.dll -o libGlutin.dll
-curl https://dl.feenk.com/Glutin/windows/development/x86_64/msvcp140.dll -o msvcp140.dll
-curl https://dl.feenk.com/Glutin/windows/development/x86_64/vcruntime140.dll -o vcruntime140.dll
+curl https://dl.feenk.com/Moz2D/windows/development/x86_64/msvcp140.dll -o msvcp140.dll
+curl https://dl.feenk.com/Moz2D/windows/development/x86_64/vcruntime140.dll -o vcruntime140.dll
 mv libGlutin.dll $GTFolder
 mv msvcp140.dll $GTFolder
 mv vcruntime140.dll $GTFolder
