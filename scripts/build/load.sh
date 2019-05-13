1f7b209b640ef0f07b45a0582c719b1ceb98d3a0
George Ganea <georgeganea@gmail.com>
2019-05-13 16:19:43 +0300

    run pharo with xvfb-run

--- scripts/build/load.sh
+++ scripts/build/load.sh
@@ -1,9 +1,6 @@
 #/bin/sh!
 set -o xtrace
-echo $DISPLAY
-export DISPLAY=:99.0
-xdpyinfo -display $DISPLAY > /dev/null || Xvfb $DISPLAY -screen 0 1024x768x16 &
 curl https://get.pharo.org/64/stable+vm | bash
-./pharo Pharo.image st --quit scripts/build/icebergconfig.st  2>&1
-./pharo Pharo.image st --quit scripts/build/loadgt.st  2>&1
+xvfb-run ./pharo Pharo.image st --quit scripts/build/icebergconfig.st  2>&1
+xvfb-run ./pharo Pharo.image st --quit scripts/build/loadgt.st  2>&1
 exit 0
--- scripts/build/runreleaser.sh
+++ scripts/build/runreleaser.sh
@@ -1,6 +1,5 @@
 #/bin/sh!
 set -o xtrace
 set -e
-echo $DISPLAY
-export DISPLAY=:99.0
-./pharo Pharo.image releasegtoolkit ${FORCED_TAG_NAME}
+
+xvfb-run ./pharo Pharo.image releasegtoolkit ${FORCED_TAG_NAME}
