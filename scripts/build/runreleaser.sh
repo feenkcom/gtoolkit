b02510fc0b3e4e4770a6fcf3d611b462c1fe2e50
Andrei Chiș <chisvasileandrei@gmail.com>
2019-06-21 11:53:29 +0200

    Disable updating of completion before running releaser

--- scripts/build/runreleaser.sh
+++ scripts/build/runreleaser.sh
@@ -2,4 +2,5 @@
 set -o xtrace
 set -e
 
+xvfb-run -a -e /dev/stdout ./pharo Pharo.image eval --save "GtPharoCompletionStrategy unsubscribeFromSystem"
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image releasegtoolkit ${FORCED_TAG_NAME}
