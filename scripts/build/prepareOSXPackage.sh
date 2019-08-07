2bf61110c6ca2271de8e8502c79dc266b698e346
George Ganea <georgeganea@gmail.com>
2019-08-07 16:16:57 +0200

    build gt with minhdls vm

--- scripts/build/prepareOSXPackage.sh
+++ scripts/build/prepareOSXPackage.sh
@@ -6,9 +6,9 @@ mkdir -p $GTFolder
 cp -rv GToolkit-64*/* $GTFolder
 rm -rf $GTFolder/pharo-local
 
-curl https://files.pharo.org/get-files/70/pharo64-mac-stable.zip -o pharo64-mac-stable.zip
-unzip pharo64-mac-stable.zip -d pharo64-mac-stable
-mv -fv pharo64-mac-stable/* $GTFolder/
+curl https://dl.feenk.com/pharovm/Pharo.app.zip -o Pharo.app.zip
+unzip Pharo.app.zip
+mv -fv Pharo.app $GTFolder/
 
 curl https://dl.feenk.com/Glutin/osx/development/x86_64/libGlutin.dylib -o libGlutin.dylib
 mv libGlutin.dylib $GTFolder
