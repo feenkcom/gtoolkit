cbc2eb0c0b7c8977f2b696226ceed674a85fb5ed
George Ganea <georgeganea@gmail.com>
2019-08-08 07:16:20 +0200

    moved windows to minhdls vm and now saving the image with GtWorld opened for all platforms

--- scripts/build/loadtag.sh
+++ scripts/build/loadtag.sh
@@ -4,4 +4,10 @@ set -o xtrace
 curl https://get.pharo.org/64/stable+vm | bash
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/icebergconfig.st  2>&1
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/loadtag.st 2>&1
+
+#download a minheadless vm and save the image with GtWorld opened.
+wget https://bintray.com/opensmalltalk/vm/download_file?file_path=pharo.cog.spur-cmake-minhdls_linux64x64_itimer_201908070737.tar.gz -O pharo.cog.spur-cmake-minhdls_linux64x64_itimer_201908070737.tar.gz
+tar xvzf pharo.cog.spur-cmake-minhdls_linux64x64_itimer_201908070737.tar.gz
+
+xvfb-run -a  ./phcogspurlinuxmhdls64/pharo Pharo.image eval "GtWorld open. Smalltalk snapshot: true andQuit: true."
 exit 0
--- scripts/build/prepareOSXPackage.sh
+++ scripts/build/prepareOSXPackage.sh
@@ -16,15 +16,6 @@ mv libGlutin.dylib $GTFolder
 curl https://dl.feenk.com/Moz2D/osx/development/x86_64/libMoz2D.dylib -o libMoz2D.dylib
 mv libMoz2D.dylib $GTFolder
 
-wget https://bintray.com/opensmalltalk/vm/download_file?file_path=pharo.cog.spur-cmake-minhdls_linux64x64_itimer_201908070737.tar.gz -O pharo.cog.spur-cmake-minhdls_linux64x64_itimer_201908070737.tar.gz
-tar xvzf pharo.cog.spur-cmake-minhdls_linux64x64_itimer_201908070737.tar.gz
-
-GTOOLKIT_IMAGE=`find $GTFolder -type f -name "GToolkit-*.image"`
-
-echo $GTOOLKIT_IMAGE
-
-xvfb-run -a  ./phcogspurlinuxmhdls64/pharo $GTOOLKIT_IMAGE eval "GtWorld open. Smalltalk snapshot: true andQuit: true."
-
 zip -qyr $GTFolder.zip $GTFolder
 rm -rf $GTFolder
 set +e
