6b99a9b0bce17d0c7a565ba1e605bbad5607be0d
Aliaksei Syrel <alex.syrel@gmail.com>
2019-08-08 11:13:50 +0200

    Add RUST_BACKTRACE=1

--- scripts/build/loadtag.sh
+++ scripts/build/loadtag.sh
@@ -1,5 +1,6 @@
 #/bin/sh!
 set -o xtrace
+export RUST_BACKTRACE=1
 
 curl https://get.pharo.org/64/stable+vm | bash
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/icebergconfig.st  2>&1
