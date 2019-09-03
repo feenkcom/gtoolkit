6532ae2f264ca73ce54cc52b25643591c6b034d4
George Ganea <georgeganea@gmail.com>
2019-09-03 14:11:55 +0300

    remove git credentials for now

--- scripts/build/macLoad.sh
+++ scripts/build/macLoad.sh
@@ -2,6 +2,5 @@
 set -o xtrace
 export RUST_BACKTRACE=1
 curl https://get.pharo.org/64/stable+vm | bash
-./pharo Pharo.image st --quit scripts/build/icebergconfig.st  2>&1
 ./pharo Pharo.image st --quit scripts/build/loadgt.st  2>&1
 exit 0
