6a0d74b5273da0fa7e5397cde26fc157983d9108
George Ganea <georgeganea@gmail.com>
2019-09-17 12:02:51 +0300

    print system info about github osx servers

--- scripts/build/macLoad.sh
+++ scripts/build/macLoad.sh
@@ -1,6 +1,7 @@
 #/bin/sh!
 set -o xtrace
 export RUST_BACKTRACE=1
-curl https://get.pharo.org/64/stable+vm | bash
-./pharo Pharo.image st --quit scripts/build/loadgt.st  2>&1
+system_profiler SPSoftwareDataType
+system_profiler SPDisplaysDataType
+defaults read /Library/Preferences/com.apple.windowserver.plist
 exit 0
