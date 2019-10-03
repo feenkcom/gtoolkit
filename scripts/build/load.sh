54d5e17e02005702f7d2886d4a67267a9acbdafa
George Ganea <georgeganea@gmail.com>
2019-10-03 10:52:09 +0300

    try with pharo 7 vm and pharo 8 latest image

--- scripts/build/load.sh
+++ scripts/build/load.sh
@@ -1,7 +1,8 @@
 #/bin/sh!
 set -o xtrace
 export RUST_BACKTRACE=1
-curl https://get.pharo.org/64/stable+vm | bash
+curl https://get.pharo.org/64/80 | bash
+curl  https://get.pharo.org/64/vm70 | bash
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/icebergconfig.st  2>&1
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/loadgt.st  2>&1
 exit 0
