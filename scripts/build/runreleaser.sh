a03d9f0c970ebe582c0b489c6d919572a68ace6c
George Ganea <georgeganea@gmail.com>
2019-10-07 13:01:00 +0300

    show new commits when a build is done

--- scripts/build/runreleaser.sh
+++ scripts/build/runreleaser.sh
@@ -2,5 +2,9 @@
 set -o xtrace
 set -e
 
+rm newcommits.txt
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image eval --save "GtPharoCompletionStrategy unsubscribeFromSystem"
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image releasegtoolkit ${FORCED_TAG_NAME}
+
+export NEWCOMMITS=$(cat newcommits.txt)
+
