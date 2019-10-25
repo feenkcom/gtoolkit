9971d14f570f45a20d44b35ea32c8c79cca811d5
George Ganea <georgeganea@gmail.com>
2019-10-25 15:19:39 +0300

    gtoolkit mac os x app as one file

--- scripts/build/prepareOSXPackage.sh
+++ scripts/build/prepareOSXPackage.sh
@@ -3,8 +3,6 @@ set -e
 
 GTFolder=GToolkitOSX64-$TAG_NAME
 mkdir -p $GTFolder
-cp -rv GToolkit-64*/* $GTFolder
-rm -rf $GTFolder/pharo-local
 
 libFolder=libOSX64-$TAG_NAME
 mkdir -p $libFolder
@@ -15,16 +13,19 @@ mkdir -p $libFolder
 curl https://dl.feenk.com/gtvm/PharoVM-8.1.0-4a6a3adc5-mac64-bin.zip -o PharoVM-8.1.0-4a6a3adc5-mac64-bin.zip
 unzip PharoVM-8.1.0-4a6a3adc5-mac64-bin.zip -d $GTFolder/
 
+cp -rv GToolkit-64*/* $GTFolder/GToolkit.app/Contents/MacOS/
+rm -rf $GTFolder/GToolkit.app/Contents/MacOS/pharo-local
+
 curl https://dl.feenk.com/Glutin/osx/development/x86_64/libGlutin.dylib -o libGlutin.dylib
-cp libGlutin.dylib $GTFolder
+cp libGlutin.dylib $GTFolder/GToolkit.app/Contents/MacOS/
 cp libGlutin.dylib $libFolder
 
 curl https://dl.feenk.com/Moz2D/osx/development/x86_64/libMoz2D.dylib -o libMoz2D.dylib
-cp libMoz2D.dylib $GTFolder
+cp libMoz2D.dylib $GTFolder/GToolkit.app/Contents/MacOS/
 cp libMoz2D.dylib $libFolder
 
 curl https://dl.feenk.com/Clipboard/osx/development/x86_64/libClipboard.dylib -o libClipboard.dylib
-cp libClipboard.dylib $GTFolder
+cp libClipboard.dylib $GTFolder/GToolkit.app/Contents/MacOS/
 cp libClipboard.dylib $libFolder
 
 zip -qyr $GTFolder.zip $GTFolder
