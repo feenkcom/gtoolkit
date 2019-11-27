b4793ee171516e3e31c26c8711dcf8be5e721fcd
George Ganea <georgeganea@gmail.com>
2019-11-27 16:06:10 +0200

    config ssh and load code in the same step

--- scripts/build/load.sh
+++ scripts/build/load.sh
@@ -2,7 +2,7 @@
 set -o xtrace
 export RUST_BACKTRACE=1
 curl https://get.pharo.org/64/80+vm| bash
-xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/icebergconfig.st  2>&1
+
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/loadgt.st  2>&1
 rm -f newcommits*
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image printNewCommits
