4eb81a0e25a19cd125dcf3816cae26779f28106e
George Ganea <georgeganea@gmail.com>
2019-11-27 16:41:26 +0200

    reduced the number of image saves

--- scripts/build/loadtag.sh
+++ scripts/build/loadtag.sh
@@ -6,7 +6,4 @@ curl https://get.pharo.org/64/80+vm | bash
 
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/loadtag.st 2>&1
 
-xvfb-run -a -e /dev/stdout ./pharo Pharo.image  st --quit scripts/build/icebergunconfig.st  2>&1
-xvfb-run -a -e /dev/stdout ./pharo Pharo.image  eval --save 'IceRepository registry removeAll.'
-
 exit 0
