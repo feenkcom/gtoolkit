5c9829728dddc440617b90553a207cfbc64f51d6
George Ganea <georgeganea@gmail.com>
2019-11-29 11:16:19 +0200

    use the fixed vm

--- scripts/build/load.sh
+++ scripts/build/load.sh
@@ -1,7 +1,11 @@
 #/bin/sh!
 set -o xtrace
 export RUST_BACKTRACE=1
-curl https://get.pharo.org/64/80+vm| bash
+curl https://get.pharo.org/64/80 | bash
+curl https://dl.feenk.com/gtvm/vmakg.tar.gz -o vmakg.tar.gz
+tar -xvf vmakg.tar.gz
+mv vmakg/* .
+rmdir vmakg
 
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/loadgt.st  2>&1
 rm -f newcommits*
