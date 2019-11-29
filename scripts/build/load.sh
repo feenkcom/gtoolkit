4458681a9fb66ec213e1e46de1113fc22e8480b5
George Ganea <georgeganea@gmail.com>
2019-11-29 13:26:06 +0200

    switch back to normal vm

--- scripts/build/load.sh
+++ scripts/build/load.sh
@@ -1,11 +1,8 @@
 #/bin/sh!
 set -o xtrace
 export RUST_BACKTRACE=1
-curl https://get.pharo.org/64/80 | bash
-wget https://bintray.com/opensmalltalk/vm/download_file\?file_path\=pharo.cog.spur_linux64x64_201911282316.tar.gz -O vmakg.tar.gz
-tar -xvf vmakg.tar.gz
-mv phcogspur64linuxht/* .
-rmdir phcogspur64linuxht
+curl https://get.pharo.org/64/80+vm | bash
+
 
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/loadgt.st  2>&1
 rm -f newcommits*
--- scripts/build/loadtag.sh
+++ scripts/build/loadtag.sh
@@ -2,11 +2,7 @@
 set -o xtrace
 export RUST_BACKTRACE=1
 
-curl https://get.pharo.org/64/80 | bash
-wget https://bintray.com/opensmalltalk/vm/download_file\?file_path\=pharo.cog.spur_linux64x64_201911282316.tar.gz -O vmakg.tar.gz
-tar -xvf vmakg.tar.gz
-mv phcogspur64linuxht/* .
-rmdir phcogspur64linuxht
+curl https://get.pharo.org/64/80+vm | bash
 
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/loadtag.st 2>&1
 
