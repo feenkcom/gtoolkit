3ba467c78d31e0856e27c6fa5b566cdd1ec435a6
George Ganea <georgeganea@gmail.com>
2019-12-02 13:39:15 +0200

    use the fixed vm

--- scripts/build/load.sh
+++ scripts/build/load.sh
@@ -1,7 +1,11 @@
 #/bin/sh!
 set -o xtrace
 export RUST_BACKTRACE=1
-curl https://get.pharo.org/64/80+vm | bash
+
+curl https://get.pharo.org/64/80 | bash
+
+curl https://dl.feenk.com/gtvm/Pharo-8.2.0-654ac0031-linux64-bin.zip -o Pharo-8.2.0-654ac0031-linux64-bin.zip
+unzip Pharo-8.2.0-654ac0031-linux64-bin.zip
 
 
 xvfb-run -a -e /dev/stdout ./pharo Pharo.image st --quit scripts/build/loadgt.st  2>&1
