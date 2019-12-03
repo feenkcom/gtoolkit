f7c1315e735a812d365c5836a260e1d071b8edee
George Ganea <georgeganea@gmail.com>
2019-12-03 16:09:59 +0200

    switch to a fixed image version

--- scripts/build/loadtag.sh
+++ scripts/build/loadtag.sh
@@ -2,7 +2,10 @@
 set -o xtrace
 export RUST_BACKTRACE=1
 
-curl https://get.pharo.org/64/80 | bash
+wget https://files.pharo.org/image/80/Pharo8.0-SNAPSHOT.build.1028.sha.327e19b.arch.64bit.zip -O Pharo.zip
+unzip Pharo.zip
+mv Pharo8.0*.image Pharo.image
+mv Pharo8.0*.changes Pharo.changes
 
 curl https://dl.feenk.com/gtvm/Pharo-8.2.0-654ac0031-linux64-bin.zip -o Pharo-8.2.0-654ac0031-linux64-bin.zip
 unzip Pharo-8.2.0-654ac0031-linux64-bin.zip
