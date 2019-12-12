630bf69aa5036627950e18629a82534dbb9349c3
George Ganea <georgeganea@gmail.com>
2019-12-12 19:20:05 +0200

    updated local script

--- scripts/localbuild/linux.sh
+++ scripts/localbuild/linux.sh
@@ -1,32 +1,15 @@
-#/bin/sh!
-set -o xtrace
+
 
 find . ! -name '*.st' ! -name '*.sh' ! -name '.' -exec rm -rf {} +
 
-GTVERSION=$(curl -s https://api.github.com/repos/feenkcom/gtoolkit/releases/latest | grep tag_name | cut -d '"' -f 4)
-TIMESTAMP=$(curl -s https://api.github.com/repos/feenkcom/gtoolkit/releases/latest | grep /GToolkit | cut -d '"' -f 4 | tail -c 19 | cut -c1-14)
+wget https://files.pharo.org/image/80/Pharo8.0-SNAPSHOT.build.1059.sha.9dd40fe.arch.64bit.zip -O Pharo.zip
+unzip Pharo.zip
+mv Pharo8.0*.image Pharo.image
+mv Pharo8.0*.changes Pharo.changes
 
-curl https://get.pharo.org/64/vm80 | bash
-
-wget https://github.com/feenkcom/gtoolkit/releases/download/$GTVERSION/GToolkit-64-$GTVERSION-$TIMESTAMP.zip -O image.zip 
-unzip -o image.zip 
-mv GToolkit-64-$GTVERSION-$TIMESTAMP/GToolkit-64-$GTVERSION-"$TIMESTAMP"64.image Pharo.image
-mv GToolkit-64-$GTVERSION-$TIMESTAMP/GToolkit-64-$GTVERSION-"$TIMESTAMP"64.changes Pharo.changes
-mv GToolkit-64-$GTVERSION-$TIMESTAMP/Pharo8*.sources .
-mv GToolkit-64-$GTVERSION-$TIMESTAMP/gt-extra gt-extra
-wget https://github.com/feenkcom/gtoolkit/releases/download/$GTVERSION/libLinux64-$GTVERSION.zip -O libLinux64-$GTVERSION.zip
-unzip -o libLinux64-$GTVERSION.zip
-mv libLinux64-$GTVERSION/lib* .
-
-./pharo Pharo.image st --quit loadsociator.st 2>&1
-
-./pharo Pharo.image sociator-es-load
+# curl https://get.pharo.org/64/80 | bash
 
 curl https://dl.feenk.com/gtvm/Pharo-8.2.0-654ac0031-linux64-bin.zip -o Pharo-8.2.0-654ac0031-linux64-bin.zip
-unzip Pharo-8.2.0-654ac0031-linux64-bin.zip -d headless
+unzip Pharo-8.2.0-654ac0031-linux64-bin.zip
 
-./headless/pharo Pharo.image eval --interactive --no-quit "SoEsAppModel openDefaultWithShutdownHandler. 60 seconds wait. BlHost pickHost universe snapshot: true andQuit: true"
-
-
-
-exit 0
+time ./pharo Pharo.image st --quit loadgt.st 2>&1
