2b3d5a1b57a9681340d92d1ab6544d03c7219023
George Ganea <georgeganea@gmail.com>
2020-01-09 20:19:25 +0200

    first enable threadedFFI then open gtworld

--- scripts/build/test.sh
+++ scripts/build/test.sh
@@ -37,7 +37,8 @@ then
   # We should also pass --no-quit flag, otherwise the VM will be terminated before the universe ever gets a chance to save an image.
   # It takes significant amount of time to start GtWorld, so let's wait for 30 seconds to make sure everything is initialized
   # There is not need to run save and quit an image from a forked process, because the save request is deffered though the universe
-  xvfb-run -a  ./gtoolkit "${ARTIFACT_DIR}/${PROJECT_NAME}64.image" eval --interactive --no-quit " ThreadedFFIMigration enableThreadedFFI. GtWorld openWithShutdownListener. 30 seconds wait. BlHost pickHost universe snapshot: true andQuit: true"
+  xvfb-run -a  ./gtoolkit "${ARTIFACT_DIR}/${PROJECT_NAME}64.image" eval --interactive --no-quit "ThreadedFFIMigration enableThreadedFFI. BlHost pickHost universe snapshot: true andQuit: true"
+  xvfb-run -a  ./gtoolkit "${ARTIFACT_DIR}/${PROJECT_NAME}64.image" eval --interactive --no-quit "GtWorld openWithShutdownListener. 30 seconds wait. BlHost pickHost universe snapshot: true andQuit: true"
 fi
 
 set +e
