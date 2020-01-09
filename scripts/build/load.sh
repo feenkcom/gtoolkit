691a0b781f7d72a82615f7dfd39b673d4b00337e
George Ganea <georgeganea@gmail.com>
2020-01-09 13:55:25 +0200

    removed some comments

--- scripts/build/load.sh
+++ scripts/build/load.sh
@@ -2,15 +2,7 @@
 set -o xtrace
 export RUST_BACKTRACE=full
 
-# wget https://files.pharo.org/image/80/Pharo8.0-SNAPSHOT.build.1059.sha.9dd40fe.arch.64bit.zip -O Pharo.zip
-# unzip Pharo.zip
-# mv Pharo8.0*.image Pharo.image
-# mv Pharo8.0*.changes Pharo.changes
-
 curl https://get.pharo.org/64/80 | bash
-
-# curl https://dl.feenk.com/gtvm/Pharo-8.2.0-654ac0031-linux64-bin.zip -o Pharo-8.2.0-654ac0031-linux64-bin.zip
-# unzip Pharo-8.2.0-654ac0031-linux64-bin.zip
 
 curl https://dl.feenk.com/gtvm/GToolkitVM-8.2.0-ea61a04db-linux64-bin.zip -o GToolkitVM-8.2.0-ea61a04db-linux64-bin.zip
 unzip GToolkitVM-8.2.0-ea61a04db-linux64-bin.zip
