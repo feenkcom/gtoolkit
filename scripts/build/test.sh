6aa771711f695f7b67c675996f99802ab5a0e495
George Ganea <georgeganea@gmail.com>
2020-01-17 08:28:20 +0200

    reduce the number of image saves

--- scripts/build/test.sh
+++ scripts/build/test.sh
@@ -20,6 +20,8 @@ cp -Rv gt-extra "${ARTIFACT_DIR}/"
 
 export build_zip="${ARTIFACT_DIR}.zip"
 
+xvfb-run -a  ./gtoolkit "${ARTIFACT_DIR}/${PROJECT_NAME}64.image" eval --save "IceCredentialsProvider sshCredentials publicKey: ''; privateKey: ''. IceCredentialsProvider useCustomSsh: false. IceRepository registry removeAll. ThreadedFFIMigration enableThreadedFFI." 
+zip -qr "$build_zip" "$ARTIFACT_DIR"
 
 # Download and run tests using the pharo64-linux-headless vm.
 
@@ -30,10 +32,6 @@ export build_zip="${ARTIFACT_DIR}.zip"
   # We should also pass --no-quit flag, otherwise the VM will be terminated before the universe ever gets a chance to save an image.
   # It takes significant amount of time to start GtWorld, so let's wait for 30 seconds to make sure everything is initialized
   # There is not need to run save and quit an image from a forked process, because the save request is deffered though the universe
-xvfb-run -a  ./gtoolkit "${ARTIFACT_DIR}/${PROJECT_NAME}64.image" eval --save "ThreadedFFIMigration enableThreadedFFI."
-xvfb-run -a  ./gtoolkit "${ARTIFACT_DIR}/${PROJECT_NAME}64.image" eval --save "IceCredentialsProvider sshCredentials publicKey: ''; privateKey: ''. IceCredentialsProvider useCustomSsh: false. IceRepository registry removeAll." 
-zip -qr "$build_zip" "$ARTIFACT_DIR"
-
 xvfb-run -a  ./gtoolkit "${ARTIFACT_DIR}/${PROJECT_NAME}64.image" eval --interactive --no-quit "GtWorld openWithShutdownListener. 30 seconds wait. BlHost pickHost universe snapshot: true andQuit: true"
 # fi
 
