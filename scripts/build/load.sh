9df332a051f3a8a85bb02d91187618b458c08891
George Ganea <georgeganea@gmail.com>
2020-01-18 12:25:30 +0200

    go back to working pharo image

--- scripts/build/load.sh
+++ scripts/build/load.sh
@@ -2,7 +2,10 @@
 set -o xtrace
 export RUST_BACKTRACE=full
 
-curl https://get.pharo.org/64/80 | bash
+curl http://files.pharo.org/image/80/Pharo8.0-SNAPSHOT.build.1116.sha.e967f28.arch.64bit.zip -o Pharo8.0-SNAPSHOT.build.1116.sha.e967f28.arch.64bit.zip
+unzip Pharo8.0-SNAPSHOT.build.1116.sha.e967f28.arch.64bit.zip
+mv Pharo8.0-SNAPSHOT-64bit-e967f28.image Pharo.image
+mv Pharo8.0-SNAPSHOT-64bit-e967f28.changes Pharo.changes
 
 sh scripts/build/downloadLatestVM.sh
 unzip build-artifacts.zip
