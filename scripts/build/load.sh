35ef32d83f3b54181edafb96b052d87f3e81a0f4
George Ganea <georgeganea@gmail.com>
2020-01-18 15:48:13 +0200

    try the last image that worked

--- scripts/build/load.sh
+++ scripts/build/load.sh
@@ -2,10 +2,11 @@
 set -o xtrace
 export RUST_BACKTRACE=full
 
-curl https://files.pharo.org/image/80/Pharo8.0-SNAPSHOT.build.1115.sha.053389c.arch.64bit.zip -o Pharo8.0-SNAPSHOT.build.1115.sha.053389c.arch.64bit.zip
-unzip Pharo8.0-SNAPSHOT.build.1115.sha.053389c.arch.64bit.zip
-mv Pharo8.0-SNAPSHOT-64bit-053389c.image Pharo.image
-mv Pharo8.0-SNAPSHOT-64bit-053389c.changes Pharo.changes
+rm -rf Pharo8.0-SNAPSHOT*
+curl https://files.pharo.org/image/80/Pharo8.0-SNAPSHOT.build.1114.sha.2258354.arch.64bit.zip -o Pharo8.0-SNAPSHOT.build.1114.sha.2258354.arch.64bit.zip
+unzip Pharo8.0-SNAPSHOT.build.1114.sha.2258354.arch.64bit.zip
+mv Pharo8.0-SNAPSHOT-64bit-*.image Pharo.image
+mv Pharo8.0-SNAPSHOT-64bit-*.changes Pharo.changes
 
 sh scripts/build/downloadLatestVM.sh
 unzip build-artifacts.zip
