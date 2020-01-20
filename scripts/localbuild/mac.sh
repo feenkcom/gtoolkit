3b90f5e68e658c8a61d5dcc70e1554462a15faa1
George Ganea <georgeganea@gmail.com>
2020-01-20 14:40:45 +0200

    code cleanup

--- scripts/localbuild/mac.sh
+++ scripts/localbuild/mac.sh
@@ -1,11 +1,8 @@
 #/bin/sh!
 set -o xtrace
 
-mv pharo-local ../
+# find . ! -name '*.st' ! -name '*.sh' ! -name '.' -exec rm -rf {} +
 
-find . ! -name '*.st' ! -name '*.sh' ! -name '.' -exec rm -rf {} +
-
-rm -rf Pharo8.0-SNAPSHOT*
 curl https://files.pharo.org/image/80/Pharo8.0-SNAPSHOT.build.1114.sha.2258354.arch.64bit.zip -o Pharo8.0-SNAPSHOT.build.1114.sha.2258354.arch.64bit.zip
 unzip Pharo8.0-SNAPSHOT.build.1114.sha.2258354.arch.64bit.zip
 mv Pharo8.0-SNAPSHOT-64bit-*.image Pharo.image
@@ -19,6 +16,6 @@ time ./GToolkit.app/Contents/MacOS/GToolkit Pharo.image st --quit loadgt.st 2>&1
 
 ./GToolkit.app/Contents/MacOS/GToolkit Pharo.image eval --save "ThreadedFFIMigration enableThreadedFFI." 
 ./GToolkit.app/Contents/MacOS/GToolkit Pharo.image eval --interactive --no-quit "GtWorld openWithShutdownListener. 30 seconds wait. BlHost pickHost universe snapshot: true andQuit: true."
-
+./GToolkit.app/Contents/MacOS/GToolkit Pharo.image
 
 exit 0
