23f10853cde63779d1be61e0bedbaaa731055cc4
George Ganea <georgeganea@gmail.com>
2020-01-22 16:27:29 +0200

    #757 do the smoke test in the ui phase

--- scripts/build/parallelsmoke/lnx_3_upload.sh
+++ scripts/build/parallelsmoke/lnx_3_upload.sh
@@ -8,4 +8,5 @@ TAG_NAME=$( cat tagname.txt )
 mv GToolkitLinux64.zip GToolkitLinux64-"${TAG_NAME}".zip 
 
 scp GToolkitLinux64-"${TAG_NAME}".zip $AWS:$GTfolder
+scp gt.jpg $AWS:$GTfolder
 rm -rf GToolkitLinux64-*
--- scripts/build/test.sh
+++ scripts/build/test.sh
@@ -33,21 +33,3 @@ xvfb-run -a -e /dev/stdout ./gtoolkit Pharo.image examples --junit-xml-output 'G
 xvfb-run -a -e /dev/stdout ./gtoolkit Pharo.image gtexportreport --report=GtGtoolkitArchitecturalReport
 
 set -e
-
-#run smoke tests
-timeout 60 xvfb-run -a ./gtoolkit "${ARTIFACT_DIR}/${PROJECT_NAME}64.image" --interactive &
-sleep 50
-export DISPLAY=$(ps -aux | grep screen -m 1 | awk '{print $12}')
-export XAUTHORITY=$(ps -aux | grep screen -m 1 | awk '{print $19}')
-# It is enough to specify -root to take a screenshot of the main screen
-xwd -display $DISPLAY -root -out gt.xwd
-convert -verbose gt.xwd gt.jpg
-
-if [ $(find gt.jpg -type f -size +20000c 2>/dev/null) ] 
-then
-    echo "Screenshot looking good."
-else 
-    ls -al
-    echo "gt.jpg does not exist or is too small. Exiting with 1 in order to fail the build."
-    exit 1
-fi
