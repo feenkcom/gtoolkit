a89be2778e7abb82d28e8c63387dfb7c5f2f23f1
George Ganea <georgeganea@gmail.com>
2020-01-23 13:35:04 +0200

    #757 get the latest tag, not the latest release

--- scripts/build/open_gt_world.sh
+++ scripts/build/open_gt_world.sh
@@ -1,5 +1,5 @@
 #/bin/sh!
 set -o xtrace
-GTVERSION=$(curl -s https://api.github.com/repos/feenkcom/gtoolkit/releases/latest | grep tag_name | cut -d '"' -f 4)
+GTVERSION=$(curl -s https://api.github.com/repos/feenkcom/gtoolkit/tags/latest | grep tag_name | cut -d '"' -f 4)
 echo $GTVERSION > tagname.txt
 xvfb-run -a -e /dev/stdout ./gtoolkit GToolkit-64-*/GToolkit-64-*.image eval --interactive --no-quit "GtWorld openWithShutdownListener. 30 seconds wait. BlHost pickHost universe snapshot: true andQuit: true"
