12e8228e57bf99651f2fac8e44dab49a7c63b9ee
George Ganea <georgeganea@gmail.com>
2020-01-27 20:16:45 +0200

    dont cat tagname

--- scripts/build/parallelsmoke/osx_2_smoke.sh
+++ scripts/build/parallelsmoke/osx_2_smoke.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -o xtrace
-TAG_NAME=$( cat tagname.txt )
+# TAG_NAME=$( cat tagname.txt )
 # echo "${SUDO}" | sudo -S ./GToolkitOSX64/GToolkit.app/Contents/MacOS/GToolkit GToolkitOSX64/GToolkit-64-*.image examples --junit-xml-output 'GToolkit-Coder-Examples' 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Sparta-.*'
 
