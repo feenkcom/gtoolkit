b1405c98d27d5f15e3a2b6c621a38a410438de96
George Ganea <georgeganea@gmail.com>
2020-01-28 11:23:30 +0200

    local script that reproduces the crash on linux

--- scripts/localbuild/linux.sh
+++ scripts/localbuild/linux.sh
@@ -1,5 +1,5 @@
 
-
+set -o xtrace
 #find . ! -name '*.st' ! -name '*.sh' ! -name '.' -exec rm -rf {} +
 
 wget https://raw.githubusercontent.com/feenkcom/gtoolkit/master/scripts/localbuild/loadgt.st -O loadgt.st
@@ -11,7 +11,7 @@ unzip build-artifacts.zip
 unzip build-artifacts/GToolkitVM-8.2.0-*-linux64-bin.zip
 
 time ./gtoolkit Pharo.image st --quit loadgt.st 2>&1
-./gtoolkit Pharo.image examples --interactive --no-quit --junit-xml-output 'GToolkit-Releaser-.*' 2>&1
-./gtoolkit Pharo.image Pharo.image eval --save "ThreadedFFIMigration enableThreadedFFI." 
+./gtoolkit Pharo.image examples --junit-xml-output  'GToolkit-Coder-Examples' 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Sparta-.*' 2>&1
+./gtoolkit Pharo.image eval --save "ThreadedFFIMigration enableThreadedFFI." 
 # ./GToolkit.app/Contents/MacOS/GToolkit Pharo.image eval --interactive --no-quit "GtWorld openWithShutdownListener. 30 seconds wait. BlHost pickHost universe snapshot: true andQuit: true."
-./gtoolkit Pharo.image examples --interactive --no-quit --junit-xml-output 'GToolkit-Releaser-.*' 2>&1
+./gtoolkit Pharo.image examples --junit-xml-output  'GToolkit-Coder-Examples' 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Sparta-.*' 2>&1
