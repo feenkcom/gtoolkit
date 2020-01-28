4110758f38a84d03c4bcb0a3feff3823a75290db
George Ganea <georgeganea@gmail.com>
2020-01-28 09:45:25 +0200

    updated local linux script so that it is zeroconf ready

--- scripts/localbuild/linux.sh
+++ scripts/localbuild/linux.sh
@@ -1,17 +1,17 @@
 
 
-find . ! -name '*.st' ! -name '*.sh' ! -name '.' -exec rm -rf {} +
+#find . ! -name '*.st' ! -name '*.sh' ! -name '.' -exec rm -rf {} +
 
-wget https://files.pharo.org/image/80/Pharo8.0-SNAPSHOT.build.1059.sha.9dd40fe.arch.64bit.zip -O Pharo.zip
-unzip Pharo.zip
-mv Pharo8.0*.image Pharo.image
-mv Pharo8.0*.changes Pharo.changes
+wget https://raw.githubusercontent.com/feenkcom/gtoolkit/master/scripts/localbuild/loadgt.st -O loadgt.st
 
-# curl https://get.pharo.org/64/80 | bash
+curl https://get.pharo.org/64/80 | bash
 
-curl https://dl.feenk.com/gtvm/Pharo-8.2.0-654ac0031-linux64-bin.zip -o Pharo-8.2.0-654ac0031-linux64-bin.zip
-unzip Pharo-8.2.0-654ac0031-linux64-bin.zip
+wget https://github.com/feenkcom/opensmalltalk-vm/releases/latest/download/build-artifacts.zip -O build-artifacts.zip
+unzip build-artifacts.zip
+unzip build-artifacts/GToolkitVM-8.2.0-*-linux64-bin.zip
 
-time ./pharo Pharo.image st --quit loadgt.st 2>&1
-
-./pharo Pharo.image examples --interactive --no-quit --junit-xml-output 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Starta-.*' 2>&1
+time ./gtoolkit Pharo.image st --quit loadgt.st 2>&1
+./gtoolkit Pharo.image examples --interactive --no-quit --junit-xml-output 'GToolkit-Releaser-.*' 2>&1
+./gtoolkit Pharo.image Pharo.image eval --save "ThreadedFFIMigration enableThreadedFFI." 
+# ./GToolkit.app/Contents/MacOS/GToolkit Pharo.image eval --interactive --no-quit "GtWorld openWithShutdownListener. 30 seconds wait. BlHost pickHost universe snapshot: true andQuit: true."
+./gtoolkit Pharo.image examples --interactive --no-quit --junit-xml-output 'GToolkit-Releaser-.*' 2>&1
