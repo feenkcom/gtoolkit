dd9e8dee83e8a775b596c9516ed253d26a840745
George Ganea <georgeganea@gmail.com>
2020-02-05 16:42:05 +0200

    take out the macosx testing for now

--- scripts/build/parallelsmoke/osx_2_smoke.sh
+++ scripts/build/parallelsmoke/osx_2_smoke.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -o xtrace
-echo "${SUDO}" | sudo -S gtimeout 6m ./GToolkitOSX64/GToolkit.app/Contents/MacOS/GToolkit GToolkitOSX64/GToolkit-64-*.image examples --junit-xml-output 'GToolkit-Coder-Examples' 'GToolkit-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Sparta-.*'
+# echo "${SUDO}" | sudo -S gtimeout 6m ./GToolkitOSX64/GToolkit.app/Contents/MacOS/GToolkit GToolkitOSX64/GToolkit-64-*.image examples --junit-xml-output 'GToolkit-Coder-Examples' 'GToolkit-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Sparta-.*'
 
