1cfa59e861a2151e5f5627e31652332397096ccb
George Ganea <georgeganea@gmail.com>
2020-03-17 13:06:05 +0200

    try to also run 'Bloc-*' examples on windows

--- scripts/build/parallelsmoke/win_2_examples.ps1
+++ scripts/build/parallelsmoke/win_2_examples.ps1
@@ -1,4 +1,4 @@
 $image=Get-Childitem -Include *.image -Recurse -Name
-.\GToolkitWin64\GToolkitConsole.exe $image examples --junit-xml-output 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-*'
+.\GToolkitWin64\GToolkitConsole.exe $image examples --junit-xml-output 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc-*' 'Bloc' 
 rm .\GToolkit-Releaser-*
 rm .\GToolkit-Documenter-XDoc-Examples.xml
