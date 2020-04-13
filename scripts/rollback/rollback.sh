32889116fc5b74f73b1d33829cc82dc7cacbffa5
George Ganea <georgeganea@gmail.com>
2020-04-13 20:36:15 +0300

    rollback website release feenkcom/gtoolkit#735

--- scripts/rollback/rollback.sh
+++ scripts/rollback/rollback.sh
@@ -1,6 +1,21 @@
 set -o xtrace
 set -e
+
 export GTfolder=/var/www/html/gt
-cat $GTfolder/GToolkitWin64-release
+
+version=$(cat $GTfolder/GToolkitWin64-release)
+vn="$(cut -d '.' -f3 <<<$version)"
+previous="$(($vn - 1))"
+sed -i "s/${vn}/${previous}/g" $GTfolder/GToolkitWin64-release
+
+version=$(cat $GTfolder/GToolkitOSX64-release)
+vn="$(cut -d '.' -f3 <<<$version)"
+previous="$(($vn - 1))"
+sed -i "s/${vn}/${previous}/g" $GTfolder/GToolkitOSX64-release
+
+version=$(cat $GTfolder/GToolkitLinux64-release)
+vn="$(cut -d '.' -f3 <<<$version)"
+previous="$(($vn - 1))"
+sed -i "s/${vn}/${previous}/g" $GTfolder/GToolkitLinux64-release
 
 set +e
