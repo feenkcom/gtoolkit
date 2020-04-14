22bbeec1b3963f88d74e3bc4f0cae5202fc7b875
George Ganea <georgeganea@gmail.com>
2020-04-14 12:56:24 +0300

    also rollback the hard linked zips feenkcom/gtoolkit#735

--- scripts/rollback/rollback.sh
+++ scripts/rollback/rollback.sh
@@ -8,16 +8,22 @@ version=$(cat $GTfolder/GToolkitWin64-release)
 vn="$(cut -d '.' -f3 <<<$version)"
 previous="$(($vn - 1))"
 sed -i "s/${vn}/${previous}/g" $GTfolder/GToolkitWin64-release
+version=$(cat $GTfolder/GToolkitWin64-release)
+cp $version $GTfolder/GToolkitWin64-release.zip
 
 version=$(cat $GTfolder/GToolkitOSX64-release)
 vn="$(cut -d '.' -f3 <<<$version)"
 previous="$(($vn - 1))"
 sed -i "s/${vn}/${previous}/g" $GTfolder/GToolkitOSX64-release
+version=$(cat $GTfolder/GToolkitOSX64-release)
+cp $version $GTfolder/GToolkitOSX64-release.zip
 
 version=$(cat $GTfolder/GToolkitLinux64-release)
 vn="$(cut -d '.' -f3 <<<$version)"
 previous="$(($vn - 1))"
 sed -i "s/${vn}/${previous}/g" $GTfolder/GToolkitLinux64-release
+version=$(cat $GTfolder/GToolkitLinux64-release)
+cp $version $GTfolder/GToolkitLinux64-release.zip
 
 stat -c %Y $(cat $GTfolder/GToolkitLinux64-release) > .releasedateinseconds
 
