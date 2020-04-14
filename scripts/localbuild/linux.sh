430afb393bf216156e0593ac475aa2865277924d
George Ganea <georgeganea@gmail.com>
2020-04-14 20:12:50 +0300

    enableThreadedFFI before loading gt in the local linux build

--- scripts/localbuild/linux.sh
+++ scripts/localbuild/linux.sh
@@ -3,6 +3,10 @@ set -o xtrace
 
 # find . ! -name '*.st' ! -name '*.sh' ! -name '.' -exec rm -rf {} +
 
+wget https://raw.githubusercontent.com/feenkcom/gtoolkit/master/scripts/localbuild/tffi.st -O tffi.st
+
+./gtoolkit Pharo.image st tffi.st --save --quit
+
 wget https://raw.githubusercontent.com/feenkcom/gtoolkit/master/scripts/localbuild/loadgt.st -O loadgt.st
 
 curl https://get.pharo.org/64/80 | bash
@@ -13,8 +17,7 @@ unzip build-artifacts/GToolkitVM-8.2.0-*-linux64-bin.zip
 
 time ./gtoolkit Pharo.image st --quit loadgt.st 2>&1
 
-echo "ThreadedFFIMigration enableThreadedFFI. Smalltalk snapshot: true andQuit: true."  > tffi.st
 echo "GtWorld openWithShutdownListener. 30 seconds wait. BlHost pickHost universe snapshot: true andQuit: true." > gtworld.st
-./gtoolkit Pharo.image st tffi.st
+
 ./gtoolkit Pharo.image st gtworld.st --interactive --no-quit 
 ./gtoolkit Pharo.image Pharo.image --no-quit --interactive
