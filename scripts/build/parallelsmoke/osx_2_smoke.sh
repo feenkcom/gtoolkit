1d3fe6174cfbcfcbfb9a4bb22ea25a9c2cb26eeb
George Ganea <georgeganea@gmail.com>
2020-04-15 12:19:12 +0300

    added -e to linux and mac scripts so that the first error in the script fails the build

--- scripts/build/parallelsmoke/osx_2_smoke.sh
+++ scripts/build/parallelsmoke/osx_2_smoke.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 set -o xtrace
-
+set -e
 echo "${SUDO}" | sudo -S /usr/local/bin/timeout 6m ./GToolkitOSX64/GToolkit.app/Contents/MacOS/GToolkit GToolkitOSX64/GToolkit-64-*.image examples --junit-xml-output 'GToolkit-Coder-Examples' 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Sparta-.*'
-
-exit 0
