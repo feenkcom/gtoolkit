e6c271937d12ac73ffabc21ac2bff1782a420219
George Ganea <georgeganea@gmail.com>
2020-05-29 11:37:03 +0300

    renamed the image and changes file

--- scripts/build/parallelsmoke/osx_2_smoke.sh
+++ scripts/build/parallelsmoke/osx_2_smoke.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -o xtrace
 set -e
-echo "${SUDO}" | sudo -S /usr/local/bin/timeout 6m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit-64-*.image examples --junit-xml-output 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Sparta-.*'
+echo "${SUDO}" | sudo -S /usr/local/bin/timeout 6m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image examples --junit-xml-output 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Sparta-.*'
