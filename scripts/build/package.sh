b12a6c16f6a0a9595d36eb7bde32f1b9e4fc97b3
George Ganea <georgeganea@gmail.com>
2020-05-29 15:00:12 +0300

    fixed the version number in the folder name

--- scripts/build/package.sh
+++ scripts/build/package.sh
@@ -3,6 +3,11 @@ set -o xtrace
 set -e
 
 TAG_NAME=$(git ls-remote --tags git@github.com:feenkcom/gtoolkit.git | grep /v0 | sort -t '/' -k 3 -V | tail -n1 |sed 's/.*\///; s/\^{}//')
+
+TAG=$(cut -d'.' -f3 <<<$TAG_NAME)
+TAG=$((TAG+1))
+TAG_NAME=$TAG
+
 echo $TAG_NAME > tagname.txt
 
 sh scripts/build/prepareWin64Package.sh
