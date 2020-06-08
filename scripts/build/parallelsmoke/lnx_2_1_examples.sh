f2b7e2363e6836fd5cb7791a6e074509d8eb069b
George Ganea <georgeganea@gmail.com>
2020-06-08 12:27:49 +0300

    also remove --no-quit

--- scripts/build/parallelsmoke/lnx_2_1_examples.sh
+++ scripts/build/parallelsmoke/lnx_2_1_examples.sh
@@ -3,6 +3,6 @@ set -o xtrace
 set -e
 echo 'cleanup previous test results'
 rm -rf *.xml
-timeout 6m xvfb-run -a ./GlamorousToolkitLinux64*/glamoroustoolkit GlamorousToolkitLinux64*/GlamorousToolkit.image examples --no-quit --junit-xml-output 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Sparta-.*'
+timeout 6m xvfb-run -a ./GlamorousToolkitLinux64*/glamoroustoolkit GlamorousToolkitLinux64*/GlamorousToolkit.image examples --junit-xml-output 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc' 'Bloc-.*' 'Sparta-.*'
 timeout 6m xvfb-run -a ./GlamorousToolkitLinux64*/glamoroustoolkit GlamorousToolkitLinux64*/GlamorousToolkit.image gtexportreport --report=GtGtoolkitArchitecturalReport
 
