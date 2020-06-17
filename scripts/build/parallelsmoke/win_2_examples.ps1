cc16ccb61cd7790f2adb5e40996ef8183db16706
George Ganea <georgeganea@gmail.com>
2020-06-17 18:56:17 +0300

    try with a powershell timeout

--- scripts/build/parallelsmoke/win_2_examples.ps1
+++ scripts/build/parallelsmoke/win_2_examples.ps1
@@ -1,6 +1,14 @@
 $image=Get-Childitem -Include *.image -Recurse -Name
 echo $image
-.\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image eval "1+2" > out.txt
+
+$timeoutSeconds = 10
+$code = {
+    .\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image eval "1+2" > out.txt
+}
+$j = Start-Job -ScriptBlock $code
+if (Wait-Job $j -Timeout $timeoutSeconds) { Receive-Job $j }
+Remove-Job -force $j
+
 cat out.txt
 # .\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image examples --junit-xml-output 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc-*' 'Bloc' 
 rm .\GToolkit-Releaser-*
