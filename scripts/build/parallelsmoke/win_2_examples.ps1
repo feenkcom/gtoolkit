84e38074370e8d3036629cca921a8593405168c0
George Ganea <georgeganea@gmail.com>
2020-06-17 19:13:14 +0300

    try with quotes

--- scripts/build/parallelsmoke/win_2_examples.ps1
+++ scripts/build/parallelsmoke/win_2_examples.ps1
@@ -3,7 +3,7 @@ ls $image
 
 $timeoutSeconds = 10
 $code = {
-    .\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image eval "1+2"
+    .\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe "$image" eval "1+2"
 }
 $j = Start-Job -ScriptBlock $code
 if (Wait-Job $j -Timeout $timeoutSeconds) { Receive-Job $j }
