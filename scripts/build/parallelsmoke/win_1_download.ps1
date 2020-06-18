8c920cbbd08a9fb84270b229a4cb3e34b91602b3
George Ganea <georgeganea@gmail.com>
2020-06-18 10:34:35 +0300

    show username and list of users

--- scripts/build/parallelsmoke/win_1_download.ps1
+++ scripts/build/parallelsmoke/win_1_download.ps1
@@ -1,8 +1,12 @@
 
 git clean -fdx
+echo $env:UserName 
+net user
 
 wget https://dl.feenk.com/tentative/GlamorousToolkitWin64.zip -OutFile GlamorousToolkitWin64.zip
 Expand-Archive GlamorousToolkitWin64.zip -DestinationPath .
+
+
 
 $acl = Get-Acl GlamorousToolkitWin64-*
 
