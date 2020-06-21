586d1ffb7a23e0cc30889da96cfd531d9a6ec5d6
George Ganea <georgeganea@gmail.com>
2020-06-21 12:41:52 +0300

    try a faster download

--- scripts/build/parallelsmoke/win_3_download.ps1
+++ scripts/build/parallelsmoke/win_3_download.ps1
@@ -1,2 +1,11 @@
 
-wget https://dl.feenk.com/tentative/GlamorousToolkitWin64.zip -OutFile GlamorousToolkitWin64.zip
+#wget https://dl.feenk.com/tentative/GlamorousToolkitWin64.zip -OutFile GlamorousToolkitWin64.zip
+
+$url = "https://dl.feenk.com/tentative/GlamorousToolkitWin64.zip"
+$output = "GlamorousToolkitWin64.zip"
+$start_time = Get-Date
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
