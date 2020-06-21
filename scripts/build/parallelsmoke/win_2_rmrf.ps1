3eb19f145d2c713dfbd7cddecc530675b1d86880
George Ganea <georgeganea@gmail.com>
2020-06-21 10:06:04 +0300

    made the win scripts actually do what they are supposed to do

--- scripts/build/parallelsmoke/win_2_rmrf.ps1
+++ scripts/build/parallelsmoke/win_2_rmrf.ps1
@@ -1,6 +1,2 @@
 
-git clean -fdx
 Get-ChildItem . | Where {$_.PSIsContainer -and ($_ -match '^GlamorousToolkitWin64-')} | Remove-Item -Recurse -Force
-
-wget https://dl.feenk.com/tentative/GlamorousToolkitWin64.zip -OutFile GlamorousToolkitWin64.zip
-Expand-Archive GlamorousToolkitWin64.zip -DestinationPath .
