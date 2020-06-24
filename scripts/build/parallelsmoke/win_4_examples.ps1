97a26cdba7d5e21c5021ee21ab4fb1100bef7ffd
George Ganea <georgeganea@gmail.com>
2020-06-24 15:05:40 +0300

    don't delete the releaser test results

--- scripts/build/parallelsmoke/win_4_examples.ps1
+++ scripts/build/parallelsmoke/win_4_examples.ps1
@@ -4,5 +4,5 @@ echo $image
 
 .\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image examples --junit-xml-output 'GToolkit-.*' 'GT4SmaCC-.*' 'DeepTraverser-.*' 'Brick' 'Brick-.*' 'Bloc-*' 'Bloc' 
 
-rm .\GToolkit-Releaser-*
+# rm .\GToolkit-Releaser-*
 rm .\GToolkit-Documenter-XDoc-Examples.xml
