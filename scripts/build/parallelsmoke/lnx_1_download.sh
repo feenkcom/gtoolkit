c53228f8b8f6032a87a279fa473544ab7fe4e4c3
George Ganea <georgeganea@gmail.com>
2020-07-20 11:00:35 +0300

    added some rm commands so that we can re-run a failed build

--- scripts/build/parallelsmoke/lnx_1_download.sh
+++ scripts/build/parallelsmoke/lnx_1_download.sh
@@ -2,4 +2,5 @@
 set -o xtrace
 set -e
 curl https://dl.feenk.com/tentative/GlamorousToolkitLinux64.zip -o GlamorousToolkitLinux64.zip
+rm -rf GlamorousToolkitLinux64-v0.*
 unzip GlamorousToolkitLinux64.zip
