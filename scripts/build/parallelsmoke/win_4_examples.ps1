5d6196fdf9ac3b1d4b07751b37b2296c693be076
George Ganea <georgeganea@gmail.com>
2020-11-03 15:51:50 +0200

    run the new examples command line handler

--- scripts/build/parallelsmoke/win_4_examples.ps1
+++ scripts/build/parallelsmoke/win_4_examples.ps1
@@ -2,8 +2,8 @@ $image=Get-Childitem -Include *.image -Recurse -Name
 
 echo $image
 
-.\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image examples --junit-xml-output $EXAMPLE_PACKAGES
-.\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image slides --junit-xml-output $EXAMPLE_PACKAGES
+.\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image dedicatedReleaseBranchExamples --junit-xml-output
+# .\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image slides --junit-xml-output $EXAMPLE_PACKAGES
 
 # rm .\GToolkit-Releaser-*
 rm .\GToolkit-Documenter-XDoc-Examples.xml
