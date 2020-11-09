df231a9f7be3dc60bc68ecc17a96d45964335ac1
George Ganea <georgeganea@gmail.com>
2020-11-09 11:17:44 +0200

    added comments

--- scripts/build/parallelsmoke/lnx_2_2_smoke.sh
+++ scripts/build/parallelsmoke/lnx_2_2_smoke.sh
@@ -2,10 +2,17 @@
 set -o xtrace
 set -e
 #run smoke tests
+
+#first start gt with a 60 second timeout and put its process in the background with an &
 timeout 60 xvfb-run -a ./GlamorousToolkitLinux64*/glamoroustoolkit GlamorousToolkitLinux64*/GlamorousToolkit.image --interactive &
+
+#then sleep for 50 seconds so that gt has time to boot and settle
 sleep 50
+
+#then take a screenshot of the screen
 export DISPLAY=$(ps -aux | grep screen -m 1 | awk '{print $12}')
 export XAUTHORITY=$(ps -aux | grep screen -m 1 | awk '{print $19}')
+
 # It is enough to specify -root to take a screenshot of the main screen
 xwd -display $DISPLAY -root -out gt.xwd
 convert -verbose gt.xwd gt.jpg
