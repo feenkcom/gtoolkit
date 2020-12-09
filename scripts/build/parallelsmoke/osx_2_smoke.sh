e72776f4a4dc60cd048058c3cf47566d8ef1349b
George Ganea <georgeganea@gmail.com>
2020-12-09 13:06:17 +0200

    try setting the keys to go around the testing issue

--- scripts/build/parallelsmoke/osx_2_smoke.sh
+++ scripts/build/parallelsmoke/osx_2_smoke.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -o xtrace
 set -e
+ssh-add -K /Users/tudor/.ssh/id_rsa
+echo "${SUDO}" | sudo -S /usr/local/bin/timeout 16m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image eval --save "IceCredentialsProvider sshCredentials publicKey: '/Users/tudor/.ssh/id_rsa.pub'; privateKey: '/Users/tudor/.ssh/id_rsa'. IceCredentialsProvider useCustomSsh: true." 
 echo "${SUDO}" | sudo -S /usr/local/bin/timeout 16m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image dedicatedReleaseBranchExamples --junit-xml-output
 echo "${SUDO}" | sudo -S /usr/local/bin/timeout 6m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image dedicatedReleaseBranchSlides --junit-xml-output
