9e889025b3fe277ccae917bc1060937a97e55026
Aliaksei Syrel <alex.syrel@gmail.com>
2020-12-14 13:33:49 +0100

    print how many spaces there are after opening a gt world

--- scripts/build/open_gt_world.sh
+++ scripts/build/open_gt_world.sh
@@ -2,5 +2,5 @@
 set -o xtrace
 set -e
 export RUST_BACKTRACE=full
-echo "GtWorld openDefault. 30 seconds wait. BlHost pickHost universe snapshot: true andQuit: true." > gtworld.st
+echo "space := GtWorld openDefault. 30 seconds wait. NonInteractiveTranscript stdout nextPutAll: 'world space id = '; nextPutAll: space id asString; cr; nextPutAll: 'spaces count = '; nextPutAll: space universe spaces size asString; cr. space universe snapshot: true andQuit: true." > gtworld.st
 xvfb-run -a -e /dev/stdout ./glamoroustoolkit GlamorousToolkit/GlamorousToolkit.image st gtworld.st --interactive --no-quit 
