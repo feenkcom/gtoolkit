bcbe66ebae624342171b3dcfff07b9c1c84452bc
George Ganea <georgeganea@gmail.com>
2021-03-06 09:07:47 +0200

    fixed the windows and linux local builds

--- scripts/localbuild/linux.sh
+++ scripts/localbuild/linux.sh
@@ -4,6 +4,7 @@ set -o xtrace
 # find . ! -name '*.st' ! -name '*.sh' ! -name '.' -exec rm -rf {} +
 
 curl -L https://raw.githubusercontent.com/feenkcom/gtoolkit/master/scripts/localbuild/loadgt.st -o loadgt.st
+curl -L https://raw.githubusercontent.com/feenkcom/gtoolkit/master/scripts/localbuild/loadice.st -o loadice.st
 
 curl https://get.pharo.org/64/80 | bash
 
--- scripts/localbuild/windows.ps1
+++ scripts/localbuild/windows.ps1
@@ -78,6 +78,10 @@ $url = "https://raw.githubusercontent.com/feenkcom/gtoolkit/master/scripts/local
 $output = "$pwd\loadgt.st"
 Get-FileFromURL $url $output
 
+$url = "https://raw.githubusercontent.com/feenkcom/gtoolkit/master/scripts/localbuild/loadice.st"
+$output = "$pwd\loadice.st"
+Get-FileFromURL $url $output
+
 
 $url = "https://files.pharo.org/get-files/80/pharo64.zip"
 $output = "$pwd\pharo64.zip"
@@ -88,6 +92,7 @@ Expand-Archive build-artifacts\GlamorousToolkitVM-*-win64-bin.zip  -DestinationP
 Expand-Archive pharo64.zip -DestinationPath GlamorousToolkit 
 Move-Item .\GlamorousToolkit\Pharo8.0-SNAPSHOT-64bit-*.image .\GlamorousToolkit\Pharo.image
 Move-Item .\GlamorousToolkit\Pharo8.0-SNAPSHOT-64bit-*.changes .\GlamorousToolkit\Pharo.changes
+.\GlamorousToolkit\GlamorousToolkitConsole.exe .\GlamorousToolkit\Pharo.image st --quit .\loadice.st
 .\GlamorousToolkit\GlamorousToolkitConsole.exe .\GlamorousToolkit\Pharo.image st --quit .\loadgt.st
 .\GlamorousToolkit\GlamorousToolkitConsole.exe .\GlamorousToolkit\Pharo.image eval --save "ThreadedFFIMigration enableThreadedFFI." 
 .\GlamorousToolkit\GlamorousToolkitConsole.exe .\GlamorousToolkit\Pharo.image eval --interactive --no-quit 'GtWorld openDefault. 30 seconds wait. BlHost pickHost universe snapshot: true andQuit: true.'
