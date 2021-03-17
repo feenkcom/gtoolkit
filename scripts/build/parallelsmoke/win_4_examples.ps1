62d4d0308728875949d242493e3c091f3573449a
Aliaksei Syrel <alex.syrel@gmail.com>
2021-03-17 12:07:57 +0100

    Update win_4_examples.ps1
    Run smoke examples from GlamorousToolkitWin folder on

--- scripts/build/parallelsmoke/win_4_examples.ps1
+++ scripts/build/parallelsmoke/win_4_examples.ps1
@@ -1,9 +1,9 @@
 $image=Get-Childitem -Include *.image -Recurse -Name
 
 echo $image
-# .\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image eval --save "IceCredentialsProvider sshCredentials publicKey: 'C:/Users/jenkins/.ssh/id_rsa.pub'; privateKey: 'C:/Users/jenkins/.ssh/id_rsa'. IceCredentialsProvider useCustomSsh: true." 
-.\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image dedicatedReleaseBranchExamples --junit-xml-output
-.\GlamorousToolkitWin64-*\GlamorousToolkitConsole.exe $image dedicatedReleaseBranchSlides --junit-xml-output
+# .\GlamorousToolkitWin\GlamorousToolkitConsole.exe $image eval --save "IceCredentialsProvider sshCredentials publicKey: 'C:/Users/jenkins/.ssh/id_rsa.pub'; privateKey: 'C:/Users/jenkins/.ssh/id_rsa'. IceCredentialsProvider useCustomSsh: true." 
+.\GlamorousToolkitWin\GlamorousToolkitConsole.exe $image dedicatedReleaseBranchExamples --junit-xml-output
+.\GlamorousToolkitWin\GlamorousToolkitConsole.exe $image dedicatedReleaseBranchSlides --junit-xml-output
 
 $FileName = ".\GToolkit-Documenter-XDoc-Examples.xml"
 if (Test-Path $FileName) 
