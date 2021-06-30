a0e7ded48b9c15448ad4a4a1ee6ffde7de0b7068
George Ganea <georgeganea@gmail.com>
2021-06-30 22:58:47 +0300

    slides need more time as well on m1

--- scripts/build/parallelsmoke/osx_2_smoke.sh
+++ scripts/build/parallelsmoke/osx_2_smoke.sh
@@ -8,4 +8,4 @@ export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
 
 echo "${SUDO}" | sudo -S timeout 5m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image eval --save "IceCredentialsProvider sshCredentials publicKey: '/Users/tudor/.ssh/id_rsa.pub'; privateKey: '/Users/tudor/.ssh/id_rsa'. IceCredentialsProvider useCustomSsh: true." 
 echo "${SUDO}" | sudo -S timeout 60m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image dedicatedReleaseBranchExamples --junit-xml-output --verbose
-echo "${SUDO}" | sudo -S timeout 6m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image dedicatedReleaseBranchSlides --junit-xml-output
+echo "${SUDO}" | sudo -S timeout 18m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image dedicatedReleaseBranchSlides --junit-xml-output
