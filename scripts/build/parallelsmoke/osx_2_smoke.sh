56908e97e712b7d77ebcf763a143a8c1b5ed5678
George Ganea <georgeganea@gmail.com>
2021-07-02 16:58:02 +0300

    set the access properties after the first start

--- scripts/build/parallelsmoke/osx_2_smoke.sh
+++ scripts/build/parallelsmoke/osx_2_smoke.sh
@@ -7,8 +7,9 @@ ls -al ./GlamorousToolkitOSX64*/
 export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
 
 #try to avoid a permissions issue that might cause pharo to crash
-echo "${SUDO}" | sudo -S chmod -R 777 ./GlamorousToolkitOSX64*/
 
+echo "${SUDO}" | sudo -S chmod -R 777 ./GlamorousToolkitOSX64*/
 echo "${SUDO}" | sudo -S /usr/local/bin/timeout 5m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image eval --save "IceCredentialsProvider sshCredentials publicKey: '/Users/tudor/.ssh/id_rsa.pub'; privateKey: '/Users/tudor/.ssh/id_rsa'. IceCredentialsProvider useCustomSsh: true." 
+echo "${SUDO}" | sudo -S chmod -R 777 ./GlamorousToolkitOSX64*/
 echo "${SUDO}" | sudo -S /usr/local/bin/timeout 60m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image dedicatedReleaseBranchExamples --junit-xml-output --verbose
 echo "${SUDO}" | sudo -S /usr/local/bin/timeout 18m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image dedicatedReleaseBranchSlides --junit-xml-output
