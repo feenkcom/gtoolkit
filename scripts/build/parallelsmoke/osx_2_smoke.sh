7cb136e7ac232f135b3caa69f861123ea91d2dce
George Ganea <georgeganea@gmail.com>
2021-07-02 15:12:11 +0300

    set permissions for the entire folder

--- scripts/build/parallelsmoke/osx_2_smoke.sh
+++ scripts/build/parallelsmoke/osx_2_smoke.sh
@@ -7,7 +7,7 @@ ls -al ./GlamorousToolkitOSX64*/
 export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
 
 #try to avoid a permissions issue that might cause pharo to crash
-echo "${SUDO}" | sudo -S chmod -R 777 ./GlamorousToolkitOSX64*/pharo-local/lepiter
+echo "${SUDO}" | sudo -S chmod -R 777 ./GlamorousToolkitOSX64*/
 
 echo "${SUDO}" | sudo -S /usr/local/bin/timeout 5m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image eval --save "IceCredentialsProvider sshCredentials publicKey: '/Users/tudor/.ssh/id_rsa.pub'; privateKey: '/Users/tudor/.ssh/id_rsa'. IceCredentialsProvider useCustomSsh: true." 
 echo "${SUDO}" | sudo -S /usr/local/bin/timeout 60m ./GlamorousToolkitOSX64*/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit GlamorousToolkitOSX64*/GlamorousToolkit.image dedicatedReleaseBranchExamples --junit-xml-output --verbose
