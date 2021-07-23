fdef6b577f73fc85810b66440ab1cb41fd84c12e
George Ganea <georgeganea@gmail.com>
2021-07-23 10:15:04 +0300

    increased timeout on linux and reduced the number of retries to 2

--- scripts/build/parallelsmoke/lnx_2_1_examples.sh
+++ scripts/build/parallelsmoke/lnx_2_1_examples.sh
@@ -4,7 +4,7 @@ set -e
 echo 'cleanup previous test results'
 rm -rf *.xml
 ldd ./GlamorousToolkitLinux64*/libSkia.so
-timeout 25m xvfb-run -a ./GlamorousToolkitLinux64*/glamoroustoolkit GlamorousToolkitLinux64*/GlamorousToolkit.image dedicatedReleaseBranchExamples --junit-xml-output --verbose
+timeout 35m xvfb-run -a ./GlamorousToolkitLinux64*/glamoroustoolkit GlamorousToolkitLinux64*/GlamorousToolkit.image dedicatedReleaseBranchExamples --junit-xml-output --verbose
 timeout 6m xvfb-run -a ./GlamorousToolkitLinux64*/glamoroustoolkit GlamorousToolkitLinux64*/GlamorousToolkit.image dedicatedReleaseBranchSlides --junit-xml-output
 timeout 6m xvfb-run -a ./GlamorousToolkitLinux64*/glamoroustoolkit GlamorousToolkitLinux64*/GlamorousToolkit.image gtexportreport --report=GtGtoolkitArchitecturalReport
 
