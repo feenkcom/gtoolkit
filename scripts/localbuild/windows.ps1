54f7b633686fa4c655f803dccb52a42aad150f0e
Aliaksei Syrel <alex.syrel@gmail.com>
2024-11-07 22:48:55 +0100

    Update windows.ps1

--- scripts/localbuild/windows.ps1
+++ scripts/localbuild/windows.ps1
@@ -1,11 +1,18 @@
-$installer = "gt-installer"
-$osArchitecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
-$installerArch = switch ($osArchitecture) {
-    "X64"  { "x86_64" }
-    "Arm64" { "aarch64" }
-    Default { throw "Your architecture is currently unsupported" }
+$installer = "gt-maestro"
+$osArchitecture = (Get-CimInstance -ClassName Win32_OperatingSystem).OSArchitecture
+
+if ($osArchitecture -match "64") {
+	if ($osArchitecture -match "ARM") {
+		$installerArch = "aarch64"
+	}
+	else {
+		$installerArch = "x86_64"
+	}
+}
+else {
+	throw "Your architecture ($osArchitecture) is currently unsupported"
 }
 
-curl -o "$installer.exe" "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-$installerArch-pc-windows-msvc.exe"
-$cmd = ".\$installer.exe local-build"
-Invoke-Expression $cmd
+
+curl -o "$installer.exe" "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/gt-installer-$installerArch-pc-windows-msvc.exe"
+Start-Process -FilePath ".\$installer.exe" -ArgumentList "local-build" -Wait -NoNewWindow
